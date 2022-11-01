a5c15a9940545ef6a9a9ccadf34dd39446a65698
isadkang <akungamef543@gmail.com>
2022-11-01 10:11:20 +0700

    new file:   css/style.css 	modified:   img/dark-mode.png 	new file:   img/heheemm.heic 	new file:   img/logo-t.jpg 	new file:   img/logo.png 	new file:   img/projek-ui-webschool.png 	modified:   img/sertifikat-cmd.png 	new file:   img/uipersonalweb.png 	modified:   index.html 	new file:   js/main.js 	deleted:    main.js 	deleted:    style.css

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// iniii eee codingan buat ngetik 
+const txtElement = ['Pelajar...', 'WebDeveloper...', 'Gamers...']
+let count = 0
+let txtIndex = 0
+let currentTxt = ''
+let words = '';
+
+(function typing () {
+
+    if(count == txtElement.length){
+        count = 0
+    }
+
+    currentTxt = txtElement[count]
+
+    words = currentTxt.slice(0, ++txtIndex)
+    document.querySelector('.typing').textContent = words;
+
+
+    if(words.length == currentTxt.length){
+        count++
+        txtIndex = 0;
+    }
+
+    setTimeout(typing, 250)
+})();
